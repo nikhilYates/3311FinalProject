26e76483eaa96a403ffd517b9c53c1c869207c5e
Ilia Azari <iliaaz@my.yorku.ca>
2024-03-18 14:34:24 -0400

    PhysicalItem implementation
    implemented the PhysicalItem class with some additional attributes and getters/setters

--- pdTwo/src/main/java/itemManagement/PhysicalItem.java
+++ pdTwo/src/main/java/itemManagement/PhysicalItem.java
@@ -1,53 +1,98 @@
 package itemManagement;
 
 public class PhysicalItem extends PhysicalItemState {
+    private String itemID;
+    private String title;
+    private String location;
+    private boolean rentalStatus; // true if currently rented, false otherwise
+    private boolean purchaseOption; // true if available for purchase, false otherwise
     private int availableCopies;
     private PhysicalItemState state;
 
-    public PhysicalItem() {
-        state = new OpenState(); // by default all items are available for rent
-        availableCopies = 20;
+    public PhysicalItem(String itemID, String title, String location, boolean purchaseOption) {
+        this.itemID = itemID;
+        this.title = title;
+        this.location = location;
+        this.purchaseOption = purchaseOption;
+        this.rentalStatus = false; // Assuming item is not rented initially
+        this.availableCopies = 20; // Default number of copies
+        this.state = new OpenState(); // By default all items are available for rent
     }
-    
+
     public void setState(PhysicalItemState state) {
-    	this.state = state;
+        this.state = state;
     }
 
     @Override
     public boolean displayState() {
-        if (availableCopies>0) {
-        	System.out.println("PI, TRUE");
-        	this.state = new OpenState();
-        	return true;
-
-        }
-        else {
-        	System.out.println("PI, FALSE");
-        	this.state = new ClosedState();
-        	return false;
+        if (availableCopies > 0) {
+            System.out.println(title + ", Available");
+            this.state = new OpenState();
+            return true;
+        } else {
+            System.out.println(title + ", Not Available");
+            this.state = new ClosedState();
+            return false;
         }
     }
 
-	@Override
-	public void rentOut(PhysicalItem item) {
-		state.rentOut(this);
-		if (state.displayState()==true) {
-			availableCopies = availableCopies -5 ; // USED TO TEST, DECREMENT BY 1 FOR FINAL PROJ.
-		}
-		if (availableCopies <= 0) {
-			this.state = new ClosedState();
-			availableCopies = 0; // USED TO TEST, VERY LIKELY WE WON'T NEED THIS FOR FINAL.
-		}
-	}
+    @Override
+    public void rentOut(PhysicalItem item) {
+        state.rentOut(this);
+        if (state.displayState()) {
+            availableCopies--; // Assuming decrement by 1 for each rental
+            rentalStatus = true;
+        }
+        if (availableCopies <= 0) {
+            this.state = new ClosedState();
+            availableCopies = 0;
+            rentalStatus = false;
+        }
+    }
 
-	@Override
-	public void returnItem(PhysicalItem item) {
-		availableCopies++;
-		state.returnItem(this);
-		
-	}
-	
-    public void printStatus() { // TEST METHOD
-        System.out.println("Number of copies: " + availableCopies);
+    @Override
+    public void returnItem(PhysicalItem item) {
+        if (rentalStatus) {
+            availableCopies++;
+            rentalStatus = false;
+            state.returnItem(this);
+        }
+    }
+
+    public void printStatus() {
+        System.out.println("Item ID: " + itemID + ", Title: " + title + ", Location: " + location + ", Copies available: " + availableCopies + ", Rental Status: " + (rentalStatus ? "Rented" : "Available"));
+    }
+
+    // Getters and Setters for the new attributes
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isPurchaseOption() {
+        return purchaseOption;
+    }
+
+    public void setPurchaseOption(boolean purchaseOption) {
+        this.purchaseOption = purchaseOption;
     }
 }
