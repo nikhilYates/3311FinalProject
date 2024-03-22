8279882d0bbb417a9a1b149865d7aa56d0157bbe
Sahajdeep Singh <impulseVD@hotmail.com>
2024-03-22 13:48:14 -0400

    Added SystemManager stuff
    yeah sysmanager

---
+++ pdTwo/src/test/java/itemManagementTests/SystemManagementTests.java
@@ -0,0 +1,48 @@
+package itemManagementTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import itemManagement.PhysicalItem;
+import userManagement.SystemManager;
+
+class SystemManagementTests {
+	
+	@BeforeEach
+	public void deleteUser () throws IOException {
+		SystemManager sysMan = SystemManager.getSystemManager(1162070547);
+		PhysicalItem item = new PhysicalItem("-2", "Screem", "Somewhere", true);
+		sysMan.deleteItem(item);
+	}
+
+	@Test
+	public void enableTest() throws IOException {
+		SystemManager sysMan = SystemManager.getSystemManager(1162070547);
+		assertTrue(sysMan.enableItem(1));
+	}
+	
+	@Test
+	public void disableTest() throws IOException {
+		SystemManager sysMan = SystemManager.getSystemManager(1162070547);
+		assertTrue(sysMan.disableItem(2));
+	}
+	
+	@Test
+	public void addTest() throws IOException {	// change itemId for each test, decrement
+		SystemManager sysMan = SystemManager.getSystemManager(1162070547);
+		PhysicalItem item = new PhysicalItem("-2", "Screem", "Somewhere", true);
+		assertTrue(sysMan.addItem(item));
+	}
+	
+	@Test
+	public void addTestFalse() throws IOException {	// doesn't work, adds duplicates 
+		SystemManager sysMan = SystemManager.getSystemManager(1162070547);
+		PhysicalItem item = new PhysicalItem("-1", "Screem", "Somewhere", true);
+		assertFalse(sysMan.addItem(item));
+	}
+
+}
