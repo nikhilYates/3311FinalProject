6bead9c58eabc4e96cf357e0f5497c85e41cf7d9
Ilia Azari <iliaaz@my.yorku.ca>
2024-03-28 12:45:14 -0400

    Updated Course and CourseTest

--- pdTwo/src/main/java/itemManagement/Course.java
+++ pdTwo/src/main/java/itemManagement/Course.java
@@ -1,23 +1,29 @@
 package itemManagement;
 
 //Assuming Course and Textbook classes are defined like this:
-public class Course implements DigitalItem{
- private String courseID;
- private String courseName;
- 
-public Course(String courseID, String courseName) {
-	// TODO Auto-generated constructor stub
-	this.courseID = courseID;
-	this.courseName = courseName;
-}
-public String getCourseID() {
-	// TODO Auto-generated method stub
-	return this.courseID;
-}
-public String getCourseName() {
-	// TODO Auto-generated method stub
-	return this.courseName;
-}
+public class Course implements DigitalItem {
+	private String courseID;
+	private String courseName;
 
- // Constructor and other methods not shown for brevity
+	public Course(String courseID, String courseName) {
+		// TODO Auto-generated constructor stub
+		this.courseID = courseID;
+		this.courseName = courseName;
+	}
+
+	public String getCourseID() {
+		return this.courseID;
+	}
+
+	public String getCourseName() {
+		return this.courseName;
+	}
+
+	public void setCourseName(String newName) {
+		this.courseName = newName;
+	}
+
+	public void setCourseID(String id) {
+		this.courseID = id;
+	}
 }
